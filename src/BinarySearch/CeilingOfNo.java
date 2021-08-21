3796c4bc0bba7a6601d756bc37b8fc432b9148d3
Himanshu <himanshumalviya100@gmail.com>
2021-08-21 07:42:41 +0530

    Arrays questions

--- src/BinarySearch/CeilingOfNo.java
+++ src/BinarySearch/CeilingOfNo.java
@@ -1,23 +1,23 @@
 package BinarySearch;
 
 public class CeilingOfNo {
-    public static int binary(int[] arr,int ele){
+    public static int binary(int[] letters,int ele){
         int start=0;
-        int end= arr.length-1;
+        int end= letters.length-1;
 
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(ele==arr[mid]){
+            if(ele==letters[mid]){
                 return mid;
             }
-            else if(ele>arr[mid]){
+            else if(ele>letters[mid]){
                 start=mid+1;
             }
             else{
                 end=mid-1;
             }
         }
-        return arr[start];
+        return letters[start];
     }
     public static void main(String[] args) {
         int[] arr={2,3,5,9,14,16,18};
